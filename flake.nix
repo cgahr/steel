83795c9db7075b4edde9a647b0e6aebd355688f8
Constantin Gahr <constantin.gahr@ipp.mpg.de>
2024-03-20 16:52:36 +0100

    add alejandra formatter and format flake.nix

--- flake.nix
+++ flake.nix
@@ -42,14 +42,16 @@
           '';
         };
     in rec {
+      formatter = pkgs.alejandra;
       packages.steel = steel;
       legacyPackages = packages;
       defaultPackage = packages.steel;
-      devShell = with pkgs; mkShell {
-        buildInputs = [cargo openssl] ++ lib.optionals stdenv.isDarwin [darwin.apple_sdk.frameworks.Security];
-        nativeBuildInputs = [
-          pkg-config
-        ];
-      };
+      devShell = with pkgs;
+        mkShell {
+          buildInputs = [cargo openssl] ++ lib.optionals stdenv.isDarwin [darwin.apple_sdk.frameworks.Security];
+          nativeBuildInputs = [
+            pkg-config
+          ];
+        };
     });
 }
